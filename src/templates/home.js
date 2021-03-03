501feaac4d97421f07dcb0710d5b7b0b258107cc
Stackbit Code Editor <support@stackbit.com>
2021-03-03 17:42:19 +0000

    home.js: changed

--- src/templates/home.js
+++ src/templates/home.js
@@ -20,7 +20,7 @@ export default class Home extends React.Component {
             <Layout {...this.props}>
                 <main className={'content' + (_.get(this.props, 'pageContext.frontmatter.page_css_class', null) ? (' ' + _.get(this.props, 'pageContext.frontmatter.page_css_class', null)) : '')}>
                     {_.map(_.get(this.props, 'pageContext.frontmatter.sections', null), (section, section_idx) => {
-                        console.log('Section = ' , );
+                        console.log('Section = ' , section , section_idx);
                         
                         let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
                         let Component = components[component];
