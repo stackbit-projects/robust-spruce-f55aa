4d876c1fe267f65b49771014df974cf82b2b67d4
Stackbit Code Editor <support@stackbit.com>
2021-03-03 09:38:26 +0000

    home.js: changed

--- src/templates/home.js
+++ src/templates/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import _ from 'lodash';
 import {graphql} from 'gatsby';
 
-import components, {Layout} from '../components/index';
+import components, {Layout ,  } from '../components/index';
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
