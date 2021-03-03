30f115eb7b2baf3ad16a6eafbf4c6ee01311e7f2
Stackbit Code Editor <support@stackbit.com>
2021-03-03 18:37:21 +0000

    product.js: changed

--- src/templates/product.js
+++ src/templates/product.js
@@ -22,6 +22,8 @@ export default class Product extends React.Component {
     render() {
         let primary_bg_img_opacity_pct = _.get(this.props, 'pageContext.site.siteMetadata.bg_image_primary_opacity', null) || 100;
         let primary_bg_img_opacity = primary_bg_img_opacity_pct * 0.01;
+        console.log('Props = ' , );
+        
         return (
             <Layout {...this.props}>
 
