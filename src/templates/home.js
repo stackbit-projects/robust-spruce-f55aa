1629fd58dde492052d2e285e28ce3e1dddc2f1e1
Stackbit Code Editor <support@stackbit.com>
2021-03-03 10:12:27 +0000

    home.js: changed

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import _ from 'lodash';
-import {graphql} from 'gatsby';
+import { graphql } from 'gatsby';
 
-import components, {Layout} from '../components/index';
+import components, { Layout } from '../components/index';
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
@@ -18,18 +18,17 @@ export default class Home extends React.Component {
     render() {
         return (
             <Layout {...this.props}>
-            <main className={'content' + (_.get(this.props, 'pageContext.frontmatter.page_css_class', null) ? (' ' + _.get(this.props, 'pageContext.frontmatter.page_css_class', null)) : '')}>
-                {_.map(_.get(this.props, 'pageContext.frontmatter.sections', null), (section, section_idx) => {
-                    let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
-                    let Component = components[component];
-                    return (
-                        <>
-                       
-                    <Component key={section_idx} {...this.props} section={section} page={this.props.pageContext} site={this.props.pageContext.site} />
-                    </>
-                    )
-                })}
-            </main>
+                <main className={'content' + (_.get(this.props, 'pageContext.frontmatter.page_css_class', null) ? (' ' + _.get(this.props, 'pageContext.frontmatter.page_css_class', null)) : '')}>
+                    {_.map(_.get(this.props, 'pageContext.frontmatter.sections', null), (section, section_idx) => {
+                        let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
+                        let Component = components[component];
+                        return (
+                            
+                                <Component key={section_idx} {...this.props} section={section} page={this.props.pageContext} site={this.props.pageContext.site} />
+                            </>
+                        )
+                    })}
+                </main>
             </Layout>
         );
     }
