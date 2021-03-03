c31597efc1ca355b5f039dbe0f41da7ec83ff313
Stackbit Code Editor <support@stackbit.com>
2021-03-03 18:27:20 +0000

    home.js: changed

--- src/templates/home.js
+++ src/templates/home.js
@@ -24,8 +24,6 @@ export default class Home extends React.Component {
 
                     if (component === 'hero_section') {
                         return (
-
-
                             <Component key={section_idx} {...this.props} section={section} page={this.props.pageContext} site={this.props.pageContext.site} />
                         )
                     }
